5537ba8f801ed9cbb90abf53c9d527d3fb5c0dd0
Alex Brouwer <brouwer.alexander@gmail.com>
2026-01-04 13:22:38 +0100

    feat(tools): enhance PHP-CS-Fixer configuration with parallel execution
    Adds `setParallelConfig` to enable parallel linting for improved performance in `.php-cs-fixer.dist.php`. Refactors configuration initialization for clarity.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 $finder = PhpCsFixer\Finder::create()
                            ->in(__DIR__ . '/packages');
 
-$config = new PhpCsFixer\Config();
-
-return $config->setRules(
-    [
-        '@PSR12' => true,
-        'strict_param' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'declare_strict_types' => true,
-    ],
-)
-              ->setFinder($finder)
-              ->setRiskyAllowed(true);
+return (new PhpCsFixer\Config())
+    ->setRules(
+        [
+            '@PSR12' => true,
+            'strict_param' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'declare_strict_types' => true,
+        ],
+    )
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
