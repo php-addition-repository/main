75cd7c0805fbf53473b304e58de1ff2101fb1068
Alex Brouwer <brouwer.alexander@gmail.com>
2026-01-04 13:54:59 +0100

    feat(core): implement `Equable` interface with tests
    Introduces the `Equable` interface to facilitate object equality comparisons through the `equals` method. Adds the `EquableObject` class as a reference implementation and comprehensive PHPUnit tests to validate its behavior. Removes the unused `Core` class from the package.

---
+++ packages/core/tests/EquableObject.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Core\Tests;
+
+use Par\Core\Equable;
+
+final readonly class EquableObject implements Equable
+{
+    public function __construct(private string|int|float $value)
+    {
+    }
+
+    public function equals(?Equable $other): bool
+    {
+        return $other instanceof EquableObject && $other->value === $this->value;
+    }
+}
---
+++ packages/core/tests/EquableTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Core\Tests;
+
+use Par\Core\Equable;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+#[CoversClass(Equable::class)]
+final class EquableTest extends TestCase
+{
+    #[Test]
+    public function itReturnsFalseWhenComparingToDifferentEquableImplementation(): void
+    {
+        $equable1 = new EquableObject('foo');
+        $equable2 = new class implements Equable {
+            public function equals(?Equable $other): bool
+            {
+                return false;
+            }
+        };
+
+        self::assertFalse($equable1->equals($equable2));
+    }
+
+    #[Test]
+    public function itReturnsFalseWhenComparingToDifferentInstanceWithDifferentValue(): void
+    {
+        $equable1 = new EquableObject('foo');
+        $equable2 = new EquableObject('bar');
+
+        self::assertFalse($equable1->equals($equable2));
+    }
+
+    #[Test]
+    public function itReturnsFalseWhenComparingToNull(): void
+    {
+        $equable = new EquableObject('foo');
+
+        self::assertFalse($equable->equals(null));
+    }
+
+    #[Test]
+    public function itReturnsTrueWhenComparingToDifferentInstanceWithSameValue(): void
+    {
+        $equable1 = new EquableObject('foo');
+        $equable2 = new EquableObject('foo');
+
+        self::assertTrue($equable1->equals($equable2));
+    }
+
+    #[Test]
+    public function itReturnsTrueWhenComparingToSameInstance(): void
+    {
+        $equable = new EquableObject('foo');
+
+        self::assertTrue($equable->equals($equable));
+    }
+}
