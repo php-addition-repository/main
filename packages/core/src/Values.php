9ae58982735b40b89a044de0b969d531206b4c15
Alex Brouwer <brouwer.alexander@gmail.com>
2026-01-04 14:48:22 +0100

    feat(core): add `Values` utility with equality methods and comprehensive tests
    Introduces the `Values` class to perform flexible value equality checks, supporting scalars, objects (via `Equable`), and date/time objects with precise comparisons. Includes helper methods for collection-based equality checks and provides extensive PHPUnit tests to validate all scenarios.

---
+++ packages/core/src/Values.php
@@ -0,0 +1,175 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Core;
+
+use DateTimeInterface;
+
+/**
+ * Class containing utility methods for determining equality of values.
+ *
+ * This class provides methods to compare values, including support for custom equality
+ * defined by the `Equable` interface and precise comparison of date/time objects. It also
+ * provides helper methods for checking equality with collections of values.
+ */
+final readonly class Values
+{
+    private const string DATE_TIME_COMPARE_FORMAT = 'Y-m-d H:i:s.u e';
+
+    /**
+     * Determines if two values should be considered equal.
+     *
+     * - If both values implement `\Par\Core\Equable` then `$value->equals($otherValue)` is used.
+     * - When both values are instances of `\DateTime` or `\DateTimeImmutable` then the moment in time they represent is compared including timezone and millisecond precision.
+     * - Otherwise a strict comparison (`$value === $otherValue`) is used.
+     *
+     * Usage:
+     * ```php
+     * if (Values::equals($a, $b)) {
+     *     // $a and $b are equal
+     * }
+     * ```
+     *
+     * @template TValue
+     *
+     * @param mixed $value The value to test
+     * @param TValue $otherValue The other value with which to compare
+     *
+     * @return bool `true` if both values should be considered equal
+     */
+    public static function equals(mixed $value, mixed $otherValue): bool
+    {
+        if ($value === $otherValue) {
+            return true;
+        }
+
+        if ($value instanceof Equable && ($otherValue instanceof Equable || null === $otherValue)) {
+            return $value->equals($otherValue);
+        }
+
+        if ($otherValue instanceof Equable && ($value instanceof Equable || null === $value)) {
+            return $otherValue->equals($value);
+        }
+
+        if ($value instanceof DateTimeInterface && $otherValue instanceof DateTimeInterface) {
+            $format = self::DATE_TIME_COMPARE_FORMAT;
+
+            return $value::class === $otherValue::class && $value->format($format) === $otherValue->format($format);
+        }
+
+        return false;
+    }
+
+    /**
+     * Determines if a value should be considered equal to __any__ of the items in the list of other values.
+     *
+     * Usage:
+     * ```php
+     * if (Values::equalsAnyIn($a, [$b, $c])) {
+     *     // When equal to $b OR $c
+     * }
+     * ```
+     *
+     * @see Values::equals
+     *
+     * @template TValue
+     *
+     * @param mixed $value The value to test
+     * @param iterable<TValue> $otherValues The list of other values with which to compare
+     *
+     * @return bool `true` if value should be considered equal to any of the items in the list of other values
+     */
+    public static function equalsAnyIn(mixed $value, iterable $otherValues): bool
+    {
+        return self::containsValue($value, $otherValues);
+    }
+
+    /**
+     * Determines if a value should be considered equal to __any__ of the other values.
+     *
+     * Usage:
+     * ```php
+     * if (Values::equalsAnyOf($a, $b, $c)) {
+     *     // When equal to $b OR $c
+     * }
+     * ```
+     *
+     * @see Values::equals
+     *
+     * @template TValue
+     *
+     * @param mixed $value The value to test
+     * @param TValue ...$otherValues The other values with which to compare
+     *
+     * @return bool `true` if value should be considered equal to any of the other values
+     */
+    public static function equalsAnyOf(mixed $value, mixed ...$otherValues): bool
+    {
+        return self::equalsAnyIn($value, $otherValues);
+    }
+
+    /**
+     * Determines if a value should be considered equal to __none__ of the items in the list of other values.
+     *
+     * Usage:
+     * ```php
+     * if (Values::equalsNoneIn($a, [$b, $c])) {
+     *     // When not equal to $b AND $c
+     * }
+     * ```
+     *
+     * @see Values::equals
+     *
+     * @template TValue
+     *
+     * @param mixed $value The value to test
+     * @param iterable<TValue> $otherValues The list of other values with which to compare
+     *
+     * @return bool `true` if value should be considered equal to __none__ of the items in the list of other values
+     */
+    public static function equalsNoneIn(mixed $value, iterable $otherValues): bool
+    {
+        return self::containsValue($value, $otherValues, false);
+    }
+
+    /**
+     * Determines if a value should be considered equal to __none__ of the other values.
+     *
+     * Usage:
+     * ```php
+     * if (Values::equalsNoneOf($a, $b, $c)) {
+     *     // When not equal to $b AND $c
+     * }
+     * ```
+     *
+     * @see Values::equals
+     *
+     * @template TValue
+     *
+     * @param mixed $value The value to test
+     * @param TValue ...$otherValues The other values with which to compare
+     *
+     * @return bool `true` if value should be considered equal to __none__ of the other values
+     */
+    public static function equalsNoneOf(mixed $value, mixed ...$otherValues): bool
+    {
+        return self::equalsNoneIn($value, $otherValues);
+    }
+
+    /**
+     * @template TValue
+     *
+     * @param iterable<TValue> $otherValues
+     */
+    private static function containsValue(mixed $value, iterable $otherValues, bool $onMatch = true): bool
+    {
+        foreach ($otherValues as $otherValue) {
+            if (self::equals($value, $otherValue)) {
+                return $onMatch;
+            }
+        }
+
+        return !$onMatch;
+    }
+}
---
+++ packages/core/tests/ValuesTest.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Core\Tests;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeZone;
+use Par\Core\Values;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * @internal
+ */
+#[CoversClass(Values::class)]
+final class ValuesTest extends TestCase
+{
+    public function testEqualsAnyIn(): void
+    {
+        self::assertTrue(Values::equalsAnyIn(1, [1, 2, 3]));
+        self::assertTrue(Values::equalsAnyIn(1, [3, 2, 1]));
+        self::assertFalse(Values::equalsAnyIn(1, [2, 3, 4]));
+        self::assertFalse(Values::equalsAnyIn(1, []));
+
+        $obj1 = new EquableObject('foo');
+        $obj2 = new EquableObject('bar');
+        self::assertTrue(Values::equalsAnyIn($obj1, [$obj2, new EquableObject('foo')]));
+    }
+
+    public function testEqualsAnyOf(): void
+    {
+        self::assertTrue(Values::equalsAnyOf(1, 1, 2, 3));
+        self::assertTrue(Values::equalsAnyOf(1, 3, 2, 1));
+        self::assertFalse(Values::equalsAnyOf(1, 2, 3, 4));
+        self::assertFalse(Values::equalsAnyOf(1));
+
+        $obj1 = new EquableObject('foo');
+        $obj2 = new EquableObject('bar');
+        self::assertTrue(Values::equalsAnyOf($obj1, $obj2, new EquableObject('foo')));
+    }
+
+    public function testEqualsNoneIn(): void
+    {
+        self::assertTrue(Values::equalsNoneIn(1, [2, 3, 4]));
+        self::assertTrue(Values::equalsNoneIn(1, []));
+        self::assertFalse(Values::equalsNoneIn(1, [1, 2, 3]));
+        self::assertFalse(Values::equalsNoneIn(1, [3, 2, 1]));
+
+        $obj1 = new EquableObject('foo');
+        $obj2 = new EquableObject('bar');
+        self::assertFalse(Values::equalsNoneIn($obj1, [$obj2, new EquableObject('foo')]));
+        self::assertTrue(Values::equalsNoneIn($obj1, [$obj2]));
+    }
+
+    public function testEqualsNoneOf(): void
+    {
+        self::assertTrue(Values::equalsNoneOf(1, 2, 3, 4));
+        self::assertTrue(Values::equalsNoneOf(1));
+        self::assertFalse(Values::equalsNoneOf(1, 1, 2, 3));
+        self::assertFalse(Values::equalsNoneOf(1, 3, 2, 1));
+
+        $obj1 = new EquableObject('foo');
+        $obj2 = new EquableObject('bar');
+        self::assertFalse(Values::equalsNoneOf($obj1, $obj2, new EquableObject('foo')));
+        self::assertTrue(Values::equalsNoneOf($obj1, $obj2));
+    }
+
+    public function testEqualsWithDateTime(): void
+    {
+        $dt1 = new DateTime('2024-01-01 10:00:00.123456', new DateTimeZone('UTC'));
+        $dt2 = new DateTime('2024-01-01 10:00:00.123456', new DateTimeZone('UTC'));
+        $dt3 = new DateTime('2024-01-01 10:00:00.123456', new DateTimeZone('Europe/Berlin'));
+        $dt4 = new DateTime('2024-01-01 10:00:00.654321', new DateTimeZone('UTC'));
+
+        self::assertTrue(Values::equals($dt1, $dt2), 'Same value, timezone and milliseconds should be equal');
+        self::assertFalse(Values::equals($dt1, $dt3), 'Different timezone should not be equal');
+        self::assertFalse(Values::equals($dt1, $dt4), 'Different milliseconds should not be equal');
+    }
+
+    public function testEqualsWithDateTimeImmutable(): void
+    {
+        $dt1 = new DateTimeImmutable('2024-01-01 10:00:00.123456', new DateTimeZone('UTC'));
+        $dt2 = new DateTimeImmutable('2024-01-01 10:00:00.123456', new DateTimeZone('UTC'));
+        $dt3 = new DateTimeImmutable('2024-01-01 10:00:00.123456', new DateTimeZone('Europe/Berlin'));
+        $dt4 = new DateTimeImmutable('2024-01-01 10:00:00.654321', new DateTimeZone('UTC'));
+
+        self::assertTrue(Values::equals($dt1, $dt2), 'Same value, timezone and milliseconds should be equal');
+        self::assertFalse(Values::equals($dt1, $dt3), 'Different timezone should not be equal');
+        self::assertFalse(Values::equals($dt1, $dt4), 'Different milliseconds should not be equal');
+    }
+
+    public function testEqualsWithDifferentTypes(): void
+    {
+        self::assertFalse(Values::equals(1, 1.0));
+        self::assertFalse(Values::equals(new stdClass(), new stdClass()));
+    }
+
+    public function testEqualsWithEquable(): void
+    {
+        $obj1 = new EquableObject('foo');
+        $obj2 = new EquableObject('foo');
+        $obj3 = new EquableObject('bar');
+
+        self::assertTrue(Values::equals($obj1, $obj1), 'Same instance should be equal');
+        self::assertTrue(Values::equals($obj1, $obj2), 'Different instances with same value should be equal');
+        self::assertFalse(
+            Values::equals($obj1, $obj3),
+            'Different instances with different values should not be equal',
+        );
+    }
+
+    public function testEqualsWithMixedDateTimeTypes(): void
+    {
+        $dt1 = new DateTimeImmutable('2024-01-01 10:00:00.123456', new DateTimeZone('UTC'));
+        $dt2 = new DateTime('2024-01-01 10:00:00.123456', new DateTimeZone('UTC'));
+
+        self::assertFalse(Values::equals($dt1, $dt2), 'DateTimeImmutable and DateTime should not be equal');
+        self::assertFalse(Values::equals($dt2, $dt1), 'DateTime and DateTimeImmutable should not be equal');
+    }
+
+    public function testEqualsWithMixedEquableAndNonEquable(): void
+    {
+        $equable = new EquableObject('foo');
+        $nonEquable = 'foo';
+
+        self::assertFalse(
+            Values::equals($equable, $nonEquable),
+            'Equable vs non-equable should not be equal (even if values match internal)',
+        );
+        self::assertFalse(Values::equals($nonEquable, $equable), 'Non-equable vs equable should not be equal');
+    }
+
+    public function testEqualsWithNulls(): void
+    {
+        self::assertTrue(Values::equals(null, null));
+        self::assertFalse(Values::equals(null, 'null'));
+        self::assertFalse(Values::equals('null', null));
+    }
+
+    public function testEqualsWithScalars(): void
+    {
+        self::assertTrue(Values::equals(1, 1));
+        self::assertTrue(Values::equals('foo', 'foo'));
+        self::assertTrue(Values::equals(true, true));
+        self::assertFalse(Values::equals(1, 2));
+        self::assertFalse(Values::equals('foo', 'bar'));
+        self::assertFalse(Values::equals(true, false));
+        self::assertFalse(Values::equals(1, '1'));
+    }
+}
