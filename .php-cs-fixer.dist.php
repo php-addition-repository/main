61634779751ae8d2ecf746a1fe1a81420b3955ec
Alex Brouwer <brouwer.alexander@gmail.com>
2026-01-04 13:55:15 +0100

    feat(tools): update PHP-CS-Fixer rules for stricter code standards
    Enhances `.php-cs-fixer.dist.php` configuration by updating to `@PER-CS3x0`, enabling `final_class` rule, adding PHPUnit-specific rules, and refining class and method casing preferences for improved code quality.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,10 +8,14 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules(
         [
-            '@PSR12' => true,
+            '@PER-CS3x0' => true,
+            'final_class' => true,
+            'single_line_empty_body' => false,
             'strict_param' => true,
-            'array_syntax' => ['syntax' => 'short'],
             'declare_strict_types' => true,
+            'php_unit_internal_class' => ['types' => ['abstract', 'final', 'normal']],
+            'php_unit_method_casing' => ['case' => 'camel_case'],
+            'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
         ],
     )
     ->setFinder($finder)
