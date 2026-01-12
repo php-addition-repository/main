036529b024420dec63bad728221bae51524e8359
Alex Brouwer <brouwer.alexander@gmail.com>
2026-01-12 10:12:45 +0100

    chore: Reorder PHP-CS-Fixer rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,19 +9,19 @@ return (new PhpCsFixer\Config())
     ->setRules(
         [
             '@PER-CS3x0' => true,
-            'final_class' => true,
-            'single_line_empty_body' => false,
-            'strict_param' => true,
             'declare_strict_types' => true,
-            'php_unit_internal_class' => ['types' => ['abstract', 'final', 'normal']],
-            'php_unit_method_casing' => ['case' => 'camel_case'],
-            'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
-            'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['class', 'function', 'const']],
             'global_namespace_import' => [
                 'import_classes' => true,
                 'import_constants' => true,
                 'import_functions' => true,
             ],
+            'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['class', 'function', 'const']],
+            'final_class' => true,
+            'single_line_empty_body' => false,
+            'strict_param' => true,
+            'php_unit_internal_class' => ['types' => ['abstract', 'final', 'normal']],
+            'php_unit_method_casing' => ['case' => 'camel_case'],
+            'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
         ],
     )
     ->setFinder($finder)
